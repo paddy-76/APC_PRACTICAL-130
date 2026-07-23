06cd3d641f4495b4e54c7a3e3dd9df06f16f1f6d
paddy-76 <pradhumnyv@gmail.com>
2026-07-23 15:20:33 +0530

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,119 @@
+                        ##BELOW AVG
+##PROGRAM 1
+"""
+n=int(input("enter a number"))
+if(n==0):
+    print("number is zero")
+else:
+    print("number is non-zero")
+"""
+#PROGRAM 2
+"""
+n1=int(input("enter number 1 :"))  
+n2=int(input("enter number 2 :"))
+if n1>n2:
+    print("n1 is largest element")
+else:
+    print("n2 is largest element")
+ """
+
+#PROGRAM 3
+"""
+n=int(input("enter a number:"))
+
+if n>0:
+    print("number is positive")
+elif n<0:
+    print("number is negative")
+else:
+    print("number is nutral")
+   """
+
+#PROGRAM 4
+"""
+ch=input("enter a char:")
+
+if ch =='a' or  ch== 'e' or ch== 'i' or ch=='o' or ch=='u':
+    print("it is a vowel")
+else:
+    print("it is constant")
+    """
+                                #AVG PROGRAMS
+
+#PROGRAM 1
+"""
+
+n=int(input("enter obtained percentage:"))
+if n>=90:
+    print("Excellent performance")
+elif n>=80:
+    print("very good performance")
+elif n>=70:
+    print("good performance")
+elif n>=60:
+    print("average performance")
+else:
+    print("Poor performance")
+    """
+
+#PROGRAM 2
+"""
+n1 =int(input("enter number1:"))
+n2 =int(input("enter number 2:"))
+n3 =int(input("enter number 3:"))
+if n1>n2 and n1>n3:
+    print("n1 is greater than n2 and n3")
+elif n2>n1 and n2>n3:
+    print("n2 is grater than  n1 andd n3")
+else:
+    print("n3 is greater than n1 and n2")
+    """
+#PROGRAM 3
+"""
+n1 =int(input("enter number1:"))
+n2 =int(input("enter number 2:"))
+n3 =int(input("enter number 3:"))
+if n1<n2 and n1<n3:
+    print("n1 is LESS than n2 and n3")
+elif n2<n1 and n2<n3:
+    print("n2 is LESS than  n1 andd n3")
+else:
+    print("n3 is LESS than n1 and n2")
+    """
+
+                                #ABOVE AVG PROBLEMS
+#PROGRAM 1
+    """
+n1=int(input("enter number:"))
+
+if n1%2==0:
+    print("number is even")
+
+else:
+    print("number is odd")
+    """
+#PROGRAM 2
+    """
+year = int(input("Enter the year:"))
+
+if year%4==0:
+    print(year,"is a leap year")
+else:
+    print(year,"not a leap year")
+    """
+#PROGRAM 3
+    """
+married_status=input("enter marrietal status:")
+gender=input("enter gender male/female:")
+age=int(input("enter a age:"))
+        
+if married_status=="Married" or (married_status=="unmarried" and gender=="male" and age>30)or (married_status=="unmarried" and gender=="feamle" and age>25):
+    print("insure")
+else:
+    print("not insured:")
+"""
+    
+
+    
+  
+    
